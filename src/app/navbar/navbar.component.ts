a4a0af764ae79e026a6b79d83da9948a0b8e45a9
ramachandran6 <ramachandranrajappan@gmail.com>
2023-06-18 19:37:38 +0530

    completed

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,4 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+
+  showNav: boolean = false
+  
+  onResize(event: any) {
+    if (event.target.innerWidth <= 991) {
+      console.log(event.target.innerWidth)
+      this.showNav=true
+    }
+    else {
+      this.showNav = false
+    }
+    
+  }
+}
